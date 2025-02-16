8d5fcd8118c08400a7a7980926704714e6e2f016
ivan-pl809 <pon20007@byui.edu>
2025-02-16 00:07:15 -0600

    commit

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
+const brandsModel = require('./brandModel');
 const partsModel = require('./partsModel');
 const userModel = require('./userModel');
 const vehicleModel = require('./vehicleModel');
+const aftermarketCompaniesModel = require('./aftermarketCompaniesModel');
+const carDealerFranchiseModel = require('./carDealerFranchiseModel');
 const dbConfig = require('../utils');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -8,13 +11,19 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
+db.brands = require('./brandModel')(mongoose);
 db.users = require('./userModel')(mongoose);
 db.parts = require('./partsModel')(mongoose);
 db.vehicles = require('./vehicleModel')(mongoose);
+db.aftermarketCompanies = require('./aftermarketCompaniesModel')(mongoose);
+db.carDealerFranchise = require('./carDealerFranchiseModel')(mongoose);
 
 module.exports = {
   db,
+  brandsModel,
   partsModel,
   userModel,
   vehicleModel,
+  aftermarketCompaniesModel,
+  carDealerFranchiseModel,
 };
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 router.use('/parts', require('./partsRoute'));
 router.use('/user', require('./userRoute'));
 router.use('/vehicle', require('./vehicleRoute'));
+router.use('/brand', require('./brandRoute'));
+router.use('/aftermarketComp', require('./aftermarketCompaniesRoute'));
+router.use('/', require('./authenticate.routes'));
 
 module.exports = router;
