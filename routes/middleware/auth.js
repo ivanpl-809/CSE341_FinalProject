a515c4731241190fa09ac587b2a6035e07f29a4c
ivan-pl809 <pon20007@byui.edu>
2025-02-17 01:40:15 -0600

    commit

--- routes/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.session.user) {
-    next();
-  } else {
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-}
-
-module.exports = { isAuthenticated };
